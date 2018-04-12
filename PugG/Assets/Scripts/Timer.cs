34c779563b19df165d81389c414b3ee003c489b9
mriviere3 <mrivie3@lsu.edu>
2018-04-12 11:07:27 -0500

    Timer
    Added a timer to the game. If time hits 0 the level will reset with a full timer. When you reach the goal bone for the end of level time will be displayed.

---
+++ PugG/Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+    public Text timer;
+    public float timeLimit;
+    private float startTime;
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        float t = timeLimit - Time.time + startTime;
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f0");
+
+        timer.text = minutes + ":" + seconds;
+
+        if (t <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+        }
+    }
+}
--- PugG/Assets/Scripts/UITimeText.cs
+++ PugG/Assets/Scripts/UITimeText.cs
@@ -3,8 +3,12 @@ using UnityEngine.UI;
 
 public class UITimeText : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		GetComponent<Text>().text = "Not implemented";
+    // Use this for initialization
+    private float startTime;
+    void Start () {
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f0");
+        GetComponent<Text>().text = minutes + ":" + seconds;
 	}
 }

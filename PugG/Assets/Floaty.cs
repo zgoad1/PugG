dbd2db9896cfc71895029beb78aef9c36ff40725
zgoad1 <zqgoad@gmail.com>
2018-04-23 16:55:33 -0500

    Make stuff float nicely

---
+++ PugG/Assets/Floaty.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floaty : MonoBehaviour {
+
+	private Vector3 newPos;
+	private float iy;
+	[SerializeField] private float amplitude = 0.1f;
+	[SerializeField] private float period = 2f;
+	private float phaseShift;
+
+	// Use this for initialization
+	void Start () {
+		newPos = transform.position;
+		iy = transform.position.y;
+		phaseShift = Random.Range(0f, period);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newPos.y = iy + amplitude * Mathf.Sin(2f * Mathf.PI / period * Time.time + phaseShift);
+		transform.position = newPos;
+	}
+}

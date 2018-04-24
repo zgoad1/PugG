94754b46e681fbb639e4947588290a7e7ef0e8b5
zgoad1 <zqgoad@gmail.com>
2018-04-23 20:54:30 -0500

    Level 2 100% polished

---
+++ PugG/Assets/OtherExecute.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherExecute : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject.Find("Tiles`").transform.Find("Tilemap_Other").gameObject.SetActive(true);
+		transform.Find("HiddenDuck").gameObject.SetActive(true);
+	}
+}

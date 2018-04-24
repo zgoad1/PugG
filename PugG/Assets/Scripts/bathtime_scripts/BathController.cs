04be9684effeea678bf283ae76127b06f9b257a5
rodomvp <vrodomista@gmail.com>
2018-04-24 01:26:37 -0500

    add bath timer

--- PugG/Assets/Scripts/bathtime_scripts/BathController.cs
+++ PugG/Assets/Scripts/bathtime_scripts/BathController.cs
@@ -2,21 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BathController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float TIME_LIMIT = 5F;
+    public Text timerText;
+    private float timer = 0F;
 
-        if(GameObject.FindWithTag("Coin") == null)
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        this.timer += Time.deltaTime;
+
+        timerText.text = timer.ToString("F0");
+
+        if (this.timer >= TIME_LIMIT)
         {
             SceneManager.LoadScene("Level1");
         }
+
+        if (GameObject.FindWithTag("Coin") == null || GameObject.FindWithTag("Player") ==  null)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+
 		
 	}
 }
